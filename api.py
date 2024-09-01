a8639133c82c3a5a6d021416f344e839282b9406
Shivam Mathur <shivammathur@SVMacBookPro.attlocal.net>
2024-08-31 21:06:24 -0500

    add cors middleware

--- api.py
+++ api.py
@@ -1,7 +1,23 @@
 from src.entity_detect import entity_detect
 from src.transcription import transcribe
 from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Add CORS middleware
+orig_origins = [
+    "http://localhost:3000",  # React app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=orig_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/extract_entities/")
 def create_item(item: dict):
